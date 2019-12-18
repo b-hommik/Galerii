8b1bf1e5b9d5638259c8bb995e0a3b79acdc64d3
B-hommik <b-hommik@hotmail.com>
2019-12-18 19:48:46 +0200

    Initial commit

---
+++ functions/function.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    i=1;
+    while (i<5){
+        source="2018/download"+i+".jpg";
+        document.getElementById("img"+i).src=source; 
+        i=i+1;   
+    }
+    console.log(source);
+}
+function esimene() {
+    i=1;
+    while (i<5){
+        source="2018/download"+i+".jpg";
+        document.getElementById("img"+i).src=source; 
+        i=i+1;   
+    }
+    console.log(source);
+}
+function teine() {
+    i=1;
+    while (i<5){
+        source="2019/download"+i+".jpg";
+        document.getElementById("img"+i).src=source; 
+        i=i+1;   
+    }
+    console.log(source);
+}
