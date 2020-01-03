5849bdd87b60d0cd6e468392c1bac6930745dfd6
thea <b-hommik@hotmail.com>
2020-01-03 15:29:44 +0200

    added new categories

--- functions/function.js
+++ functions/function.js
@@ -1,12 +1,12 @@
 window.onload=tagElements();
 function tagElements(){
 
-    var item=document.getElementsByClassName("firstButton");
+    var item = document.getElementsByClassName("btn btn-outline-secondary");
     for(var i =0; i<item.length;i++){
         item[i].addEventListener("click", generateImages); 
     }
 }
-function esimene() {
+/*function esimene() {
     i=1;
     while (i<5){
         source="2018/download"+i+".jpg";
@@ -23,7 +23,7 @@ function teine() {
         i=i+1;   
     }
     console.log(source);
-}
+}*/
 function generateImages(){
     
     //var year=document.getElementById("firstButton").innerHTML;
@@ -47,6 +47,7 @@ function generateImages(){
     }
     console.log(this.innerHTML);
 }
+/*
 function generateImages2019(){
     i=1;
     if(document.getElementById("img4")){//parem on kontrollida koige viimast elementi, mitte esimest.
@@ -64,7 +65,7 @@ function generateImages2019(){
     image.src="2019/download"+i+".jpg";
     i=i+1;
     }
-}
+}*/
 var muudetud=false;
 function onclick(){
     if(!muudetud){
@@ -72,18 +73,29 @@ function onclick(){
         this.style.left="20%";
         this.style.top="55%";
         this.style.width="700px";
-        this.style.height="500px";
-        muudetud=true;
-        console.log(this.style.width=="700px")
+        this.style.height = "500px";
+        var button = document.createElement("button");
+        document.getElementById("col-sm-9").appendChild(button);
+        button.id = "close";
+        button.type = "button";
+        button.class = "btn btn-outline-secondary";
+        document.getElementById("close").innerHTML ="close";
+        button.addEventListener("click", close);
 
+        muudetud=true;
+        console.log(this.style.width == "700px")
     }
-    else {
-        this.style.position="static";
-        this.style.left="0%";
-        this.style.top="0%";
-        this.style.width="200px";
-        this.style.height="200px"; 
+
+
+}
+function close(){
+        this.image.style.position="static";
+        this.image.style.left="0%";
+        this.image.style.top="0%";
+        this.image.style.width="200px";
+        this.image.style.height = "200px";
+        console.log(this);
         muudetud=false; 
     }
-   console.log(this);
-  }
+   //console.log(this);
+ // }
